822fb381e6a0220144248dcaeda904a83bb49050
abeshi03 <k_913411@yahoo.co.jp>
2022-02-10 00:17:09 +0900

    スクロールの処理を追加

--- src/components/CardsFlow/CardsFlow.tsx
+++ src/components/CardsFlow/CardsFlow.tsx
@@ -1,5 +1,5 @@
 // - フレームワーク =======================================================================================================
-import React, { memo, VFC } from "react";
+import React, { memo, useEffect, useRef, VFC } from "react";
 
 // - アセット ============================================================================================================
 import styles from "./CardsFlow.module.scss";
@@ -10,12 +10,46 @@ import { Card } from "../Card/Card";
 
 export const CardsFlow: VFC = memo(() => {
 
+  let scrollContainer: HTMLDivElement;
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  const scrollLeft = (): void => {
+    scrollContainer.scroll({
+      left: scrollContainer.scrollLeft - scrollContainer.clientWidth,
+      behavior: "smooth"
+    });
+  }
+
+  const scrollRight = (): void => {
+    scrollContainer.scroll({
+      left: scrollContainer.scrollLeft + scrollContainer.clientWidth,
+      behavior: "smooth"
+    });
+  }
+
+  useEffect(() => {
+    scrollContainer = ref!.current!;
+  },[])
 
   return (
-    <div className={styles.cardsFlow}>
-      <Card
-        className={styles.card}
-      />
-    </div>
+    <>
+      <div className={styles.cardsFlow}>
+        <div
+          ref={ref}
+          className={styles.scrollingContainer}
+        >
+
+          {[...Array(10)].map((_, index) => (
+            <Card
+              key={index}
+              className={styles.card}
+            />
+          ))}
+
+        </div>
+      </div>
+      <button onClick={scrollLeft}>左スクロール</button>
+      <button onClick={scrollRight}>右スクロール</button>
+    </>
   );
 });
