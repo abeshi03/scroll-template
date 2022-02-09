62ae91828bb7f023e19c800a1736945875b4acd0
abeshi03 <k_913411@yahoo.co.jp>
2022-02-10 00:54:07 +0900

    スクロールロジックを追加

--- src/components/CardsFlow/CardsFlow.tsx
+++ src/components/CardsFlow/CardsFlow.tsx
@@ -1,5 +1,5 @@
 // - フレームワーク =======================================================================================================
-import React, { memo, useEffect, useRef, VFC } from "react";
+import React, {memo, useEffect, useRef, useState, VFC} from "react";
 
 // - アセット ============================================================================================================
 import styles from "./CardsFlow.module.scss";
@@ -8,16 +8,30 @@ import styles from "./CardsFlow.module.scss";
 import { Card } from "../Card/Card";
 
 
+
+// - コンポーネント =======================================================================================================
 export const CardsFlow: VFC = memo(() => {
 
-  let scrollContainer: HTMLDivElement;
   const ref = useRef<HTMLDivElement | null>(null);
+  const [ isDisplayScrollingRightButton, setIsDisplayScrollingRightButton ] = useState(true);
+  const [ isDisplayScrollingLeftButton, setIsDisplayScrollingLeftButton ] = useState(false);
+
+
+  let scrollContainer!: HTMLDivElement;
 
   const scrollLeft = (): void => {
     scrollContainer.scroll({
       left: scrollContainer.scrollLeft - scrollContainer.clientWidth,
       behavior: "smooth"
     });
+
+    console.log("スクロール左！")
+
+    setIsDisplayScrollingRightButton(true);
+
+    if (scrollContainer.scrollLeft <= scrollContainer.clientWidth) {
+      setIsDisplayScrollingLeftButton(false);
+    }
   }
 
   const scrollRight = (): void => {
@@ -25,31 +39,52 @@ export const CardsFlow: VFC = memo(() => {
       left: scrollContainer.scrollLeft + scrollContainer.clientWidth,
       behavior: "smooth"
     });
+
+    setIsDisplayScrollingLeftButton(true);
+
+    if (
+      scrollContainer.scrollWidth - (scrollContainer.scrollLeft + scrollContainer.clientWidth) <=
+      scrollContainer.clientWidth
+    ) {
+      setIsDisplayScrollingRightButton(false);
+    }
   }
+
 
   useEffect(() => {
     scrollContainer = ref!.current!;
-  },[])
+  },[ isDisplayScrollingRightButton, isDisplayScrollingLeftButton ])
 
   return (
     <>
       <div className={styles.cardsFlow}>
-        <div
-          ref={ref}
-          className={styles.scrollingContainer}
-        >
 
+        <div
+          className={`${styles.btn} ${styles.scrollingLeftButton}`}
+          onClick={scrollLeft}
+          style={{display: isDisplayScrollingLeftButton ? "inline-block" : "none"}}
+          role="button"
+          aria-label="カードを左へスクロールする">{'<'}
+        </div>
+        <div
+          className={`${styles.btn} ${styles.scrollingRightButton}`}
+          style={{display: isDisplayScrollingRightButton ? "inline-block" : "none"}}
+          onClick={scrollRight}
+          role="button"
+          aria-label="カードを右へスクロールする">{'>'}
+        </div>
+
+
+        <div ref={ref} className={styles.scrollingContainer}>
           {[...Array(10)].map((_, index) => (
             <Card
               key={index}
               className={styles.card}
             />
           ))}
-
         </div>
+
       </div>
-      <button onClick={scrollLeft}>左スクロール</button>
-      <button onClick={scrollRight}>右スクロール</button>
     </>
   );
 });
